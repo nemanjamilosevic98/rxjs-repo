2cb2e11b9461d91ecac8a8336cc9ea6cc75f8479
nemanjamilosevic98 <nemanjamilosevic70@gmail.com>
2023-01-08 16:35:06 +0100

    Subjects part1

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import './style.css';
 import { observables } from './observables/observables';
 import { hotVsColdObservables } from './observables/hot-vs-cold-observables';
+import { subjects } from './subjects/subjects';
 
 // Observables
-observables();
+// observables();
 
 // Hot vs Cold Observables
 // hotVsColdObservables();
+
+// Subjects
+subjects();
---
+++ subjects/subjects.ts
@@ -0,0 +1,20 @@
+import { Subject } from 'rxjs';
+
+export function subjects() {
+  const subject = new Subject();
+
+  subject.next('first message');
+
+  const subscription = subject.subscribe({
+    next: (message) => console.log(message),
+    error: (error) => console.log(error),
+    complete: () => console.log('Completed'),
+  });
+
+  // subscription will get only those values that were emitted after its creation
+  // 'first message' will not be printed
+  subject.next('second message');
+  subject.next('third message');
+
+  subscription.unsubscribe();
+}
