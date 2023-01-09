b1f9f44fd00862197d97c20f4fde3867afaac51d
nemanjamilosevic98 <nemanjamilosevic70@gmail.com>
2023-01-09 12:34:03 +0100

    RxJS operators (initital commit)

--- index.ts
+++ index.ts
@@ -4,6 +4,16 @@ import { hotVsColdObservables } from './observables/hot-vs-cold-observables';
 import { subjects } from './subjects/subjects';
 import { behaviorSubjects } from './subjects/behavior-subjects';
 import { replaySubjects } from './subjects/replay-subjects';
+import { creationOperators } from './operators/creation/creation-operators';
+import { joinCreationOperators } from './operators/join-creation/join-creation-operators';
+import { transformatonOperators } from './operators/transformation/transformation-operators';
+import { filteringOperators } from './operators/filtering/filtering-operators';
+import { joinOperators } from './operators/join/join-operators';
+import { multicastingOperators } from './operators/multicasting/multicasting-operators';
+import { errorHandlingOperators } from './operators/error-handling/error-handling-operators';
+import { utilityOperators } from './operators/utility/utility-operators';
+import { conditionalAndBooleanOperators } from './operators/conditional-boolean/conditional-boolean-operators';
+import { mathAndAggregOperators } from './operators/mathematical-aggregate/math-aggreg-operators';
 
 // Observables
 // observables();
@@ -18,4 +28,34 @@ import { replaySubjects } from './subjects/replay-subjects';
 // behaviorSubjects();
 
 // Replay Subjects
-replaySubjects();
+// replaySubjects();
+
+// Creation Operators
+// creationOperators();
+
+// Join-Creation Operators
+// joinCreationOperators();
+
+// Transformation Operators
+// transformatonOperators();
+
+// Filtering Operators
+// filteringOperators();
+
+// Join Operators
+// joinOperators();
+
+// Multicasting Operators
+// multicastingOperators();
+
+// Error-Handling Operators
+// errorHandlingOperators();
+
+// Utility Operators
+// utilityOperators();
+
+// Conditional-Boolean Operators
+// conditionalAndBooleanOperators();
+
+// Mathematical-Aggregate Operators
+// mathAndAggregOperators();
