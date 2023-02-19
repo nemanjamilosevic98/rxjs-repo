52eb8bee729c071b6b6c785c1f94426d3e458b7e
nemanjamilosevic98 <nemanjamilosevic70@gmail.com>
2023-02-19 02:53:41 +0100

    join-creation operators (part 1)

--- operators/creation/creation-operators.ts
+++ operators/creation/creation-operators.ts
@@ -13,321 +13,362 @@ import {
   takeUntil,
   throwError,
   timer,
+  first,
 } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 export function operatorOf() {
-  handleOfClick();
-  // (emits each argument in whole as a separate value)
-  of(1, [1, 2, 3], 'Nemanja').subscribe((x) => console.log(x));
+  if (handleOfClick()) {
+    // (emits each argument in whole as a separate value)
+    of(1, [1, 2, 3], 'Nemanja').subscribe((x) => console.log(x));
+  }
 }
 
 function handleOfClick() {
-  console.log('%c Creation Operator of:', 'color:#add929');
   const descriptionElem = document.getElementById('creation-of-description');
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator of:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorRange() {
-  handleRangeClick();
-  // (emits a sequence of numbers in a range)
-  range(1, 5).subscribe((x) => console.log(x));
+  if (handleRangeClick()) {
+    // (emits a sequence of numbers in a range)
+    range(1, 5).subscribe((x) => console.log(x));
+  }
 }
 
 function handleRangeClick() {
-  console.log('%c Creation Operator range:', 'color:#add929');
   const descriptionElem = document.getElementById('creation-range-description');
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator range:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorFrom() {
-  handleFromClick();
-  // from (array -> Observable)
-  from([
-    {
-      name: 'Nemanja',
-      team: 'zuti',
-    },
-    { name: 'Teodora', team: 'zuti' },
-  ]).subscribe((x) => console.log(x));
-}
-
-function handleFromClick() {
-  console.log('%c Creation Operator from:', 'color:#add929');
-  const descriptionElem = document.getElementById('creation-from-description');
-  if (descriptionElem.style.display === 'none') {
-    descriptionElem.style.display = 'block';
-  } else {
-    descriptionElem.style.display = 'none';
+  if (handleFromClick()) {
+    // from (array -> Observable)
+    from([
+      {
+        name: 'Nemanja',
+        team: 'zuti',
+      },
+      { name: 'Teodora', team: 'zuti' },
+    ]).subscribe((x) => console.log(x));
   }
 }
 
+function handleFromClick() {
+  const descriptionElem = document.getElementById('creation-from-description');
+  if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator from:', 'color:#add929');
+    descriptionElem.style.display = 'block';
+    return true;
+  } else {
+    descriptionElem.style.display = 'none';
+  }
+  return false;
+}
+
 export function operatorFromEvent() {
-  handleFromEventClick();
-  // fromEvent (to listen specific events on target element)
-  fromEvent(document, 'click')
-    .pipe(takeUntil(timer(5000))) // unsubscribe after 5s
-    .subscribe((event) => console.log(event));
+  if (handleFromEventClick()) {
+    // fromEvent (to listen specific events on target element)
+    fromEvent(document, 'click')
+      .pipe(first()) // unsubscribe after 5s
+      .subscribe((event) => console.log(event));
+  }
 }
 
 function handleFromEventClick() {
-  console.log('%c Creation Operator fromEvent:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'creation-fromEvent-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator fromEvent:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorFromEventPattern() {
-  handleFromEventPatternClick();
-  fromEventPattern(addClickHandler, removeClickHandler).subscribe((x) =>
-    console.log(x)
-  );
+  if (handleFromEventPatternClick()) {
+    fromEventPattern(addClickHandler, removeClickHandler)
+      .pipe(first())
+      .subscribe((x) => console.log(x));
+  }
 }
 
 function handleFromEventPatternClick() {
-  console.log('%c Creation Operator fromEventPattern:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'creation-fromEventPattern-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator fromEventPattern:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorTimer() {
-  handleTimerClick();
-  // timer (creates an observable that will wait for a specified time period, before emitting the number 0)
-  timer(2000).subscribe((x) => console.log('timer1 emits:' + x));
-  // emit 0, 1, 2,... after every second, starting from now
-  timer(0, 1000)
-    .pipe(takeUntil(timer(5000)))
-    .subscribe((x) => console.log('timer2 emits:' + x));
-  // emit 0, 1, 2... after every second, starting 0,5s from now
-  timer(500, 1000)
-    .pipe(takeUntil(timer(5000)))
-    .subscribe((x) => console.log('timer3 emits:' + x));
-}
-
-function handleTimerClick() {
-  console.log('%c Creation Operator timer:', 'color:#add929');
-  const descriptionElem = document.getElementById('creation-timer-description');
-  if (descriptionElem.style.display === 'none') {
-    descriptionElem.style.display = 'block';
-  } else {
-    descriptionElem.style.display = 'none';
+  if (handleTimerClick()) {
+    // timer (creates an observable that will wait for a specified time period, before emitting the number 0)
+    timer(2000).subscribe((x) => console.log('timer1 emits:' + x));
+    // emit 0, 1, 2,... after every second, starting from now
+    timer(0, 1000)
+      .pipe(takeUntil(timer(5000)))
+      .subscribe((x) => console.log('timer2 emits:' + x));
+    // emit 0, 1, 2... after every second, starting 0,5s from now
+    timer(500, 1000)
+      .pipe(takeUntil(timer(5000)))
+      .subscribe((x) => console.log('timer3 emits:' + x));
   }
 }
 
+function handleTimerClick() {
+  const descriptionElem = document.getElementById('creation-timer-description');
+  if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator timer:', 'color:#add929');
+    descriptionElem.style.display = 'block';
+    return true;
+  } else {
+    descriptionElem.style.display = 'none';
+  }
+  return false;
+}
+
 export function operatorInterval() {
-  handleIntervalClick();
-  // interval (every x ms emit i++, i=0 initially)
-  // ----------------------------
-  // every 500ms emit i++
-  const sub1 = interval(500).subscribe((i) => console.log('i=' + i));
-  setTimeout(() => {
-    sub1.unsubscribe(); // stop emision after 2000ms
-  }, 2000);
-  // ----------------------------
-  setTimeout(() => {
-    interval(500) // every 500ms emit j++
-      .pipe(takeUntil(timer(2500))) // stop emision after 2500ms
-      .subscribe((j) => console.log('j=' + j));
-  }, 2000);
-  // ----------------------------
+  if (handleIntervalClick()) {
+    // interval (every x ms emit i++, i=0 initially)
+    // ----------------------------
+    // every 500ms emit i++
+    const sub1 = interval(500).subscribe((i) => console.log('i=' + i));
+    setTimeout(() => {
+      sub1.unsubscribe(); // stop emision after 2000ms
+    }, 2000);
+    // ----------------------------
+    setTimeout(() => {
+      interval(500) // every 500ms emit j++
+        .pipe(takeUntil(timer(2500))) // stop emision after 2500ms
+        .subscribe((j) => console.log('j=' + j));
+    }, 2000);
+    // ----------------------------
+  }
 }
 
 function handleIntervalClick() {
-  console.log('%c Creation Operator interval:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'creation-interval-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator interval:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorGenerate() {
-  handleGenerateClick();
-  // generate (use it instead of for loop)
-  generate({
-    initialState: 0,
-    condition: (x: number) => x < 5,
-    iterate: (x: number) => x + 1,
-  }).subscribe((x) => console.log(x));
+  if (handleGenerateClick()) {
+    // generate (use it instead of for loop)
+    generate({
+      initialState: 0,
+      condition: (x: number) => x < 5,
+      iterate: (x: number) => x + 1,
+    }).subscribe((x) => console.log(x));
+  }
 }
 
 function handleGenerateClick() {
-  console.log('%c Creation Operator generate:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'creation-generate-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator generate:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorIif() {
-  handleIifClick();
-  // iif (Change at runtime which Observable will be subscribed, depending on condition)
-  let subscribeToFirst;
-  // arguments: condition, trueResultObservable, falseResultObservable
-  const firstOrSecond = iif(() => subscribeToFirst, of('first'), of('second'));
+  if (handleIifClick()) {
+    // iif (Change at runtime which Observable will be subscribed, depending on condition)
+    let subscribeToFirst;
+    // arguments: condition, trueResultObservable, falseResultObservable
+    const firstOrSecond = iif(
+      () => subscribeToFirst,
+      of('first'),
+      of('second')
+    );
 
-  subscribeToFirst = true; // first observable will be subscribed
-  firstOrSecond.subscribe((value) => console.log(value));
+    subscribeToFirst = true; // first observable will be subscribed
+    firstOrSecond.subscribe((value) => console.log(value));
 
-  subscribeToFirst = false; // second observable will be subscribed
-  firstOrSecond.subscribe((value) => console.log(value));
+    subscribeToFirst = false; // second observable will be subscribed
+    firstOrSecond.subscribe((value) => console.log(value));
+  }
 }
 
 function handleIifClick() {
-  console.log('%c Creation Operator iif:', 'color:#add929');
   const descriptionElem = document.getElementById('creation-iif-description');
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator iif:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorAjax() {
-  handleAjaxClick();
-  // using ajax() to fetch the response object that is being returned from API
-  const obs1$ = ajax('https://api.github.com/users?per_page=5').pipe(
-    map((userResponse) => userResponse.response),
-    catchError((error) => {
-      console.log('error: ', error);
-      return of(error);
-    })
-  );
+  if (handleAjaxClick()) {
+    // using ajax() to fetch the response object that is being returned from API
+    const obs1$ = ajax('https://api.github.com/users?per_page=5').pipe(
+      map((userResponse) => userResponse.response),
+      catchError((error) => {
+        console.log('error: ', error);
+        return of(error);
+      })
+    );
 
-  obs1$.subscribe({
-    next: (value) => console.log('ajax response1:', value),
-    error: (err) => console.log(err),
-  });
+    obs1$.subscribe({
+      next: (value) => console.log('ajax response1:', value),
+      error: (err) => console.log(err),
+    });
 
-  // using ajax.getJSON() to fetch data from API
-  const obs2$ = ajax.getJSON('https://api.github.com/users?per_page=5').pipe(
-    catchError((error) => {
-      console.log('error: ', error);
-      return of(error);
-    })
-  );
+    // using ajax.getJSON() to fetch data from API
+    const obs2$ = ajax.getJSON('https://api.github.com/users?per_page=5').pipe(
+      catchError((error) => {
+        console.log('error: ', error);
+        return of(error);
+      })
+    );
 
-  obs2$.subscribe({
-    next: (value) => console.log('ajax response2:', value),
-    error: (err) => console.log(err),
-  });
+    obs2$.subscribe({
+      next: (value) => console.log('ajax response2:', value),
+      error: (err) => console.log(err),
+    });
 
-  // using ajax() with object as argument and method POST with a two seconds delay
-  const obs3$ = ajax({
-    url: 'https://httpbin.org/delay/2', // 2 sec delay
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'rxjs-custom-header': 'Rxjs',
-    },
-    body: {
-      rxjs: 'Hello World!',
-    },
-  }).pipe(
-    map((fullResponse) => fullResponse.response),
-    catchError((error) => {
-      console.log('error: ', error);
-      return of(error);
-    })
-  );
+    // using ajax() with object as argument and method POST with a two seconds delay
+    const obs3$ = ajax({
+      url: 'https://httpbin.org/delay/2', // 2 sec delay
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'rxjs-custom-header': 'Rxjs',
+      },
+      body: {
+        rxjs: 'Hello World!',
+      },
+    }).pipe(
+      map((fullResponse) => fullResponse.response),
+      catchError((error) => {
+        console.log('error: ', error);
+        return of(error);
+      })
+    );
 
-  // Using ajax() to fetch. An error object that is being returned from the request
-  obs3$.subscribe({
-    next: (value) => console.log('ajax response3: ', value),
-    error: (err) => console.log(err),
-  });
+    // Using ajax() to fetch. An error object that is being returned from the request
+    obs3$.subscribe({
+      next: (value) => console.log('ajax response3: ', value),
+      error: (err) => console.log(err),
+    });
 
-  const obs4$ = ajax('https://api.github.com/404').pipe(
-    map((userResponse) => console.log('users: ', userResponse)),
-    catchError((error) => {
-      return of(error);
-    })
-  );
+    const obs4$ = ajax('https://api.github.com/404').pipe(
+      map((userResponse) => console.log('users: ', userResponse)),
+      catchError((error) => {
+        return of(error);
+      })
+    );
 
-  obs4$.subscribe({
-    next: (value) => console.log('error: ', value),
-    error: (err) => console.log(err),
-  });
+    obs4$.subscribe({
+      next: (value) => console.log('error: ', value),
+      error: (err) => console.log(err),
+    });
+  }
 }
 
 function handleAjaxClick() {
-  console.log('%c Creation Operator ajax:', 'color:#add929');
   const descriptionElem = document.getElementById('creation-ajax-description');
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator ajax:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorBindCallback() {
-  handleBindCallbackClick();
-  function sayHello(cb) {
-    cb(1 + 2);
+  if (handleBindCallbackClick()) {
+    function sayHello(cb) {
+      cb(1 + 2);
+    }
+    const boundSomeFunction = bindCallback(sayHello);
+    boundSomeFunction().subscribe((v) => {
+      console.log(v);
+    });
   }
-  const boundSomeFunction = bindCallback(sayHello);
-  boundSomeFunction().subscribe((v) => {
-    console.log(v);
-  });
 }
 
 function handleBindCallbackClick() {
-  console.log('%c Creation Operator bindCallback:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'creation-bindCallback-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator bindCallback:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorThrowError() {
-  handleThrowErrorClick();
-  const result = throwError(() => new Error(' An error is occurred'));
-  result.subscribe({
-    next: (message) => console.log(message),
-    error: (error) => console.log(error),
-  });
+  if (handleThrowErrorClick()) {
+    const result = throwError(() => new Error(' An error is occurred'));
+    result.subscribe({
+      next: (message) => console.log(message),
+      error: (error) => console.log(error),
+    });
+  }
 }
 
 function handleThrowErrorClick() {
-  console.log('%c Creation Operator throwError:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'creation-throwError-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Creation Operator throwError:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 // ------------------------------------------------------------------
--- operators/join-creation/join-creation-operators.ts
+++ operators/join-creation/join-creation-operators.ts
@@ -1,120 +1,164 @@
-import { timer, combineLatest, takeUntil, map } from 'rxjs';
+import {
+  timer,
+  combineLatest,
+  takeUntil,
+  map,
+  interval,
+  range,
+  concat,
+  forkJoin,
+  of,
+} from 'rxjs';
 
 export function operatorCombineLatest() {
-  handleCombineLatestClick();
-  // Combines multiple Observables to create an Observable whose values are calculated from the latest values of each of its input Observables
-  const firstTimer = timer(0, 1000); // emit 0, 1, 2... after every second, starting from now
-  // emit 0, 1, 2... after every second, starting 0,5s from now
-  const secondTimer = timer(500, 1000);
-  combineLatest([firstTimer, secondTimer])
-    .pipe(takeUntil(timer(5000)))
-    .subscribe((value) => console.log(value));
+  if (handleCombineLatestClick()) {
+    // Combines multiple Observables to create an Observable whose values are calculated from the latest values of each of its input Observables
+    const firstTimer = timer(0, 1000); // emit 0, 1, 2... after every second, starting from now
+    // emit 0, 1, 2... after every second, starting 0,5s from now
+    const secondTimer = timer(500, 1000);
+    combineLatest([firstTimer, secondTimer])
+      .pipe(takeUntil(timer(5000)))
+      .subscribe((value) => console.log(value));
+  }
 }
 
 function handleCombineLatestClick() {
-  console.log('%c Join-Creation Operator combineLatest:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'join-creation-combineLatest-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Join-Creation Operator combineLatest:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorConcat() {
-  handleConcatClick();
+  if (handleConcatClick()) {
+    const timerObs = interval(1000).pipe(takeUntil(timer(4000)));
+    const rangeObs = range(500, 5);
+    const result = concat(timerObs, rangeObs);
+    result.subscribe((x) => console.log(x));
+  }
 }
 
 function handleConcatClick() {
-  console.log('%c Join-Creation Operator concat:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'join-creation-concat-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Join-Creation Operator concat:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorForkJoin() {
-  handleForkJoinClick();
+  if (handleForkJoinClick()) {
+    const observable = forkJoin({
+      foo: of(1, 2, 3, 4),
+      bar: Promise.resolve(8),
+      baz: timer(4000),
+    });
+    observable.subscribe({
+      next: (value) => console.log(value),
+      complete: () => console.log('This is how it ends!'),
+    });
+  }
 }
 
 function handleForkJoinClick() {
-  console.log('%c Join-Creation Operator forkJoin:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'join-creation-forkJoin-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Join-Creation Operator forkJoin:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorMerge() {
-  handleMergeClick();
+  if (handleMergeClick()) {
+  }
 }
 
 function handleMergeClick() {
-  console.log('%c Join-Creation Operator merge:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'join-creation-merge-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Join-Creation Operator merge:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorPartition() {
-  handlePartitionClick();
+  if (handlePartitionClick()) {
+  }
 }
 
 function handlePartitionClick() {
-  console.log('%c Join-Creation Operator partition:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'join-creation-partition-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Join-Creation Operator partition:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorRace() {
-  handleRaceClick();
+  if (handleRaceClick()) {
+  }
 }
 
 function handleRaceClick() {
-  console.log('%c Join-Creation Operator race:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'join-creation-race-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Join-Creation Operator race:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
 
 export function operatorZip() {
-  handleZipClick();
+  if (handleZipClick()) {
+  }
 }
 
 function handleZipClick() {
-  console.log('%c Join-Creation Operator zip:', 'color:#add929');
   const descriptionElem = document.getElementById(
     'join-creation-zip-description'
   );
   if (descriptionElem.style.display === 'none') {
+    console.log('%c Join-Creation Operator zip:', 'color:#add929');
     descriptionElem.style.display = 'block';
+    return true;
   } else {
     descriptionElem.style.display = 'none';
   }
+  return false;
 }
